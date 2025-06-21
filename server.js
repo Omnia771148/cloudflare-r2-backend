a08a2e90d9878e033b5709ccadd60cd434076320
Heriyuru Venkata Sai Krishna <omnia771148@gmail.com>
2025-06-21 20:20:11 +0530

    Initial commit

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.post("/get-upload-url", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 // Corrected template string syntax
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
